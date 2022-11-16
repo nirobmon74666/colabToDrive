b8668ae3adee93c290cbaa08a5f9da8557f6fc40
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-11-17 00:06:41 +0600

    Create driveUload.py

---
+++ driveUload.py
@@ -0,0 +1,52 @@
+from urllib.request import urlopen
+from tqdm.auto import tqdm
+import requests
+import shutil
+import os
+%cd //content/drive/Shareddrives/Movies/
+parDirNames= ["Movies","Anime","TV Series","Files"]
+typ="type"
+
+def downloadFile(url):
+  with requests.get(url, stream=True) as r:
+      total_length = int(r.headers.get("Content-Length"))
+      with tqdm.wrapattr(r.raw, "read", total=total_length, desc="")as raw:
+          with open(urlopen(url).headers.get_filename().replace("MLWBD.com ",""), 'wb')as output:
+              shutil.copyfileobj(raw, output)
+
+def folderDer(ftype):
+    foldername=parDirNames[ftype-1]
+    return foldername
+
+def folderList():
+  print("Folder Lists: ")
+  next(os.walk('.'))[1]
+  i=0
+  while i<len(next(os.walk('.'))[1]):
+    print("{}. {}".format((i+1),next(os.walk('.'))[1][i]))
+    i=i+1
+
+def fFolderList():
+  os.listdir()
+  i=0
+  while i<len(os.listdir()):
+    print("{}. {}".format((i+1),os.listdir()[i]))
+    i=i+1
+
+condtn=0
+url=input("Enter file URL: ")
+while condtn<2:
+  condtn+=1
+  folderList()
+  folSel=int(input("Which {} file you want to upload? :".format(typ).lower()))
+  typ=next(os.walk('.'))[1][folSel-1]
+  print(typ)
+  if typ=="Movies":
+    os.chdir(next(os.walk('.'))[1][folSel-1])
+    downloadFile(url)
+    break
+  else:
+    os.chdir(next(os.walk('.'))[1][folSel-1])
+downloadFile(url)
+fFolderList()
+
